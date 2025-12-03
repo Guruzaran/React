84dc3ac8c91622fbea9aec24481450b706e72d8f
Guruzaran <guruzaran@gmail.com>
2025-12-03 11:53:19 -0500

    No Page Found added

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,18 @@
+import { Header } from "../components/Header";
+import "./NotFoundPage";
+
+export function NotFoundPage() {
+  return (
+    <>
+      <title>Page Not Found</title>
+      <Header />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+        <p>404 Error found.The page you're looking for doesn't exist.</p>
+      </div>
+    </>
+  );
+}
