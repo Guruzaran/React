d52b15ccced8209c34c647bf71a2fb59ed426b45
Guruzaran <guruzaran@gmail.com>
2025-12-03 11:23:02 -0500

    Split header into component and used Link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,5 @@
-import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
