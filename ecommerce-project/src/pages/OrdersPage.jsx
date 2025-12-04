3f4150c73ca6ee32b50d55ee4221a55db5bec05f
Guruzaran <guruzaran@gmail.com>
2025-12-03 21:05:06 -0500

    Generate the HTML for Delivery options

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,12 +1,11 @@
 import "./OrdersPage.css";
 import { Header } from "../components/Header";
 
-export function OrdersPage() {
-  console.log("reached");
+export function OrdersPage({ cart }) {
   return (
     <>
       <title>Orders</title>
-      <Header />
+      <Header cart={cart} />
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
