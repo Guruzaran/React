1bab788e1d357c22246880d681870629767b823e
Guruzaran <guruzaran@gmail.com>
2025-12-05 11:10:39 -0500

    Update Integeration testing

---
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -0,0 +1,6 @@
+import { it, expect, describe } from "vitest";
+import { Product } from "./Product";
+
+describe("Product Component", () => {
+  it("displays the product details correctly", () => {});
+});
